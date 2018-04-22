f1760195f5e8e4fbbc9bf82b063c15beccab5897
charlie8612 <charlie88162@gmail.com>
2018-04-22 17:37:52 +0800

    ABC

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ var bot = linebot({
 });
 
 bot.on('message', function(event) {
-  console.log("GG"); //把收到訊息的 event 印出來看看
+    console.log("message, event trigger!");
+    console.log(event); //把收到訊息的 event 印出來看看
 });
 
 const app = express();
 const linebotParser = bot.parser();
 app.post('/', linebotParser);
+
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen(process.env.PORT || 8080, function() {
