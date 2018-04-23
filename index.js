9b72ceb95fd6261ea18bfbcdb42d056ed0298a21
charlie8612 <charlie88162@gmail.com>
2018-04-23 17:42:48 +0800

    ABC

--- index.js
+++ index.js
@@ -1,109 +1,43 @@
-// -----------------------------------------------------------------------------
-// モジュールのインポート
+"use strict";
+
+/*
+** Import Packages
+*/
 const server = require("express")();
-const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
-const dialogflow = require("apiai-promisified");
-const linebot = require('linebot');
-// -----------------------------------------------------------------------------
-//console.log(process.env.LINE_ACCESS_TOKEN);
-//console.log(process.env.LINE_CHANNEL_SECRET);
-// パラメータ設定
-const line_config = {
-    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
-    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
-};
+const bot_express = require("bot-express");
 
-// -----------------------------------------------------------------------------
-// Webサーバー設定
-var server_port = server.listen(process.env.PORT || 8080, function() {
-  var the_web_port = server_port.address().port;
-  console.log("App now running on port", the_web_port);
+/*
+** Middleware Configuration
+*/
+server.listen(process.env.PORT || 5000, () => {
+    console.log("server is running...");
 });
-// APIコールのためのクライアントインスタンスを作成
-const bot = new line.Client(line_config);
 
-// Dialogflowのクライアントインスタンスを作成
-const nlu = new dialogflow(process.env.DIALOGFLOW_CLIENT_ACCESS_TOKEN, {language: "en"});
-
-
-var line_bot = linebot({
-  channelId: "1575634603",
-  channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
-  channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
-});
-line_bot.listen('/linewebhook', 3000);
-const linebotParser = line_bot.parser();
-// -----------------------------------------------------------------------------
-// ルーター設定
-server.post('/', line.middleware(line_config), (req, res, next) => {
-    console.log("in post");
-    // 先行してLINE側にステータスコード200でレスポンスする。
-    res.sendStatus(200);
-
-    // thread
-    let events_processed = [];
-    // イベントオブジェクトを順次処理。
-    req.body.events.forEach((event) => {
-        // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
-        if (event.type == "message" && event.message.type == "text"){
-            events_processed.push(
-                nlu.textRequest(event.message.text, {sessionId: event.source.userId}).then((response) => {
-                    if (response.result && response.result.action == "handle-delivery-order"){
-                        let message;
-                        if (response.result.parameters.menu && response.result.parameters.menu != ""){
-                            message = {
-                                type: "text",
-                                text: `毎度！${response.result.parameters.menu}ね。どちらにお届けしましょ？`
-                            }
-                        } else {
-                            message = {
-                                type: "text",
-                                text: `HIHI!!`
-                            }
-                        }
-                        return bot.replyMessage(event.replyToken, message);
-                    }
-                })
-            );
+/*
+** Mount bot-express
+*/
+server.use("/webhook", bot_express({
+    nlu: {
+        type: "dialogflow",
+        options: {
+            client_access_token: process.env.DIALOGFLOW_CLIENT_ACCESS_TOKEN,
+            language: "ja"
         }
-    });
+    },
+    memory: {
+        type: "memory-cache",
+        retention: 600
+    },
+    line_channel_secret: process.env.LINE_CHANNEL_SECRET,
+    line_access_token: process.env.LINE_ACCESS_TOKEN,
+    facebook_app_secret: process.env.FACEBOOK_APP_SECRET,
+    facebook_page_access_token: [
+        {page_id: process.env.FACEBOOK_PAGE_ID, page_access_token: process.env.FACEBOOK_PAGE_ACCESS_TOKEN}
+    ],
+    default_skill: process.env.DEFAULT_SKILL,
+    google_project_id: process.env.GOOGLE_PROJECT_ID,
+    google_api_key: process.env.GOOGLE_API_KEY,
+    auto_translation: process.env.AUTO_TRANSLATION
+}));
 
-    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
-    Promise.all(events_processed).then(
-        (response) => {
-            console.log(`${response.length} event(s) processed.`);
-        }
-    );
-});
-/*var linebot = require('linebot');
-var express = require('express');
-
-var bot = linebot({
-  channelId: "1575634603",
-  channelSecret: channel Secret,
-  channelAccessToken: channel Access Token
-});
-
-bot.on('message', function(event) {
-    console.log("message, event trigger!");
-    event.reply(event.message.text).then(function (data) {
-    // success
-    console.log(event);
-    }).catch(function (error) {
-    // error
-    console.log("ERROR!");
-    });
-});
-
-bot.listen('/linewebhook', 3000);
-
-const app = express();
-const linebotParser = bot.parser();
-app.post('/', linebotParser);
-
-
-//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
-var server_port = app.listen(process.env.PORT || 8080, function() {
-  var port = server_port.address().port;
-  console.log("App now running on port", port);
-});*/
+module.exports = server;
---
+++ index.old.js
@@ -0,0 +1,109 @@
+// -----------------------------------------------------------------------------
+// モジュールのインポート
+const server = require("express")();
+const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
+const dialogflow = require("apiai-promisified");
+const linebot = require('linebot');
+// -----------------------------------------------------------------------------
+//console.log(process.env.LINE_ACCESS_TOKEN);
+//console.log(process.env.LINE_CHANNEL_SECRET);
+// パラメータ設定
+const line_config = {
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
+    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
+};
+
+// -----------------------------------------------------------------------------
+// Webサーバー設定
+var server_port = server.listen(process.env.PORT || 8080, function() {
+  var the_web_port = server_port.address().port;
+  console.log("App now running on port", the_web_port);
+});
+// APIコールのためのクライアントインスタンスを作成
+const bot = new line.Client(line_config);
+
+// Dialogflowのクライアントインスタンスを作成
+const nlu = new dialogflow(process.env.DIALOGFLOW_CLIENT_ACCESS_TOKEN, {language: "zh-tw"});
+
+
+var line_bot = linebot({
+  channelId: "1575634603",
+  channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
+  channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
+});
+line_bot.listen('/linewebhook', 3000);
+const linebotParser = line_bot.parser();
+// -----------------------------------------------------------------------------
+// ルーター設定
+server.post('/', line.middleware(line_config), (req, res, next) => {
+    console.log("in post");
+    // 先行してLINE側にステータスコード200でレスポンスする。
+    res.sendStatus(200);
+
+    // thread
+    let events_processed = [];
+    // イベントオブジェクトを順次処理。
+    req.body.events.forEach((event) => {
+        // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+        if (event.type == "message" && event.message.type == "text"){
+            events_processed.push(
+                nlu.textRequest(event.message.text, {sessionId: event.source.userId}).then((response) => {
+                    if (response.result && response.result.action == "handle-delivery-order"){
+                        let message;
+                        if (response.result.parameters.menu && response.result.parameters.menu != ""){
+                            message = {
+                                type: "text",
+                                text: `毎度！${response.result.parameters.menu}ね。どちらにお届けしましょ？`
+                            }
+                        } else {
+                            message = {
+                                type: "text",
+                                text: `HIHI!!`
+                            }
+                        }
+                        return bot.replyMessage(event.replyToken, message);
+                    }
+                })
+            );
+        }
+    });
+
+    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
+    Promise.all(events_processed).then(
+        (response) => {
+            console.log(`${response.length} event(s) processed.`);
+        }
+    );
+});
+/*var linebot = require('linebot');
+var express = require('express');
+
+var bot = linebot({
+  channelId: "1575634603",
+  channelSecret: channel Secret,
+  channelAccessToken: channel Access Token
+});
+
+bot.on('message', function(event) {
+    console.log("message, event trigger!");
+    event.reply(event.message.text).then(function (data) {
+    // success
+    console.log(event);
+    }).catch(function (error) {
+    // error
+    console.log("ERROR!");
+    });
+});
+
+bot.listen('/linewebhook', 3000);
+
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
+
+
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server_port = app.listen(process.env.PORT || 8080, function() {
+  var port = server_port.address().port;
+  console.log("App now running on port", port);
+});*/
